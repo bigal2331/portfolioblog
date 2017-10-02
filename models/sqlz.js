874de0b2e63f6fa3a3f9b728be7a625e9035d3fc
jnoriega <bigal2331@yahoo.com>
2017-10-02 20:05:13 +0000

    bulletin board completed

--- models/sqlz.js
+++ models/sqlz.js
@@ -1,12 +1,28 @@
 var Sequelize = require("sequelize")
 
-var sequelize = new Sequelize('postgres://ubuntu@localhost:5432/msg_app');
+// var sequelize = new Sequelize('postgres://ubuntu@localhost:5432/msg_app');
+var sequelize = new Sequelize({
+    username: 'ubuntu',
+    password: 'Letmeinnow!',
+    dialect: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    database: 'msg_app'
+});
 
 var Msg = sequelize.define('msg',{
     
-    title: Sequelize.STRING,
-    body: Sequelize.STRING,
+    title: { 
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    body: { 
+        type: Sequelize.STRING,
+        allowNull: false
+    },
     created: Sequelize.DATE
-})
+});
+
+Msg.sync()
 
 module.exports = Msg;
--- server.js
+++ server.js
@@ -1,22 +1,47 @@
 var express = require('express');
-var app = express();
-var sequelize = require("/models/sqlz");
-app.set('views', __dirname + '/views');
-app.engine('html', require('ejs').renderFile);
+var Msg = require("./models/sqlz");
+var bodyParser = require('body-parser');
+var path = require("path")
 
+var app = express();
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
 
 
 app.get('/', function(req, res){
     
-    res.render('index.html');
+   Msg.findAll().then(function(rows){
+       
+        
+        
+        res.render('index',{messages:rows});
+        
+    })
     
 })
 
 app.get('/new', function(req, res){
     
-    res.render('new.html');
+    res.render('new');
     
 })
+
+app.post('/new', function(req, res){
+    
+    Msg.sync().then(function(){
+        
+        Msg.create({
+            title: req.body.title,
+            body: req.body.msg
+        });
+        res.redirect('/')
+    });
+    
+});
+
 app.listen(8080,function(){
   
   console.log("Server listening at", process.env.PORT);
