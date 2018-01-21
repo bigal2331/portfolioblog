fc09ae0b184bd4b80c6b67b293d4491b967688ac
Jeiner Noriega <bigal2331@users.noreply.github.com>
2018-01-21 16:42:34 -0500

    setting up for heroku

--- models/sqlz.js
+++ models/sqlz.js
@@ -2,12 +2,12 @@ var Sequelize = require("sequelize")
 
 // var sequelize = new Sequelize('postgres://ubuntu@localhost:5432/msg_app');
 var sequelize = new Sequelize({
-    username: 'postgres',
-    password: 'Letmeinnow!',
+    username: 'rjlqooaqnaiitl',
+    password: '8f3ea1f41a135811503cf57d7bbe737f9cff9a5a14787145e195d91747b9f89c',
     dialect: 'postgres',
-    host: 'localhost',
+    host: 'ec2-54-221-198-206.compute-1.amazonaws.com',
     port: 5432,
-    database: 'blog'
+    database: 'ddr1276qdtnvo8'
 });
 
 var Msg = sequelize.define('msg',{
